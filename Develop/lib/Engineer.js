c9d8c552e18c33c780daf0e0c400458425557860
larry laconi <soverylarry@gmail.com>
2020-07-13 22:00:06 -0400

    adding update to Employee and Engineer

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+// add engineer class as an extension of employee class with it's own constructor
+class Engineer
+extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+  getRole() {
+    return "Engineer";
+  }
+  getGithub() {
+    return this.github;
+  }
+}
+module.exports = Engineer;
