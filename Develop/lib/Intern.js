71df45ad879d7365c813db6f3abe92ab57984310
larry laconi <soverylarry@gmail.com>
2020-07-14 19:46:54 -0400

    updating Internjs file

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,5 +1,4 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-
 const Employee = require("./Employee")
 // add Intern class as an extension of employee class with it's own constructor
 class Intern
