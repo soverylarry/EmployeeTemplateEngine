0c14eabc24281f330201516f8b8d87f4cf7d3bc9
larry laconi <soverylarry@gmail.com>
2020-07-14 18:21:37 -0400

    adding Intern and Manager js files

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee")
+// add Intern class as an extension of employee class with it's own constructor
+class Intern
+extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+  getRole() {
+    return "Intern";
+  }
+  getGithub() {
+    return this.github;
+  }
+}
+module.exports = Intern;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+// add Manager class as an extension of employee class with it's own constructor
+class Manager
+extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+  getRole() {
+    return "Manager";
+  }
+  getGithub() {
+    return this.github;
+  }
+}
+module.exports = Manager;
